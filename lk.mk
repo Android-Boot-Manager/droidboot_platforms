cfb4b808345e5f56ba8d4090c70096dcbab30c04
luka177 <lukapanio@gmail.com>
2022-05-07 16:03:13 +0300

    Initial lk makefile

--- lk.mk
+++ lk.mk
@@ -1,1 +1,25 @@
-#!/bin/bash
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+INCLUDES += $(LOCAL_PATH)/lk/common
+
+ifeq ($(PLATFORM),yggdrasil)
+	include $(LOCAL_PATH)/lk/common/common.mk
+	include $(LOCAL_PATH)/lk/common_mtk/common_mtk.mk
+	include $(LOCAL_PATH)/lk/yggdrasil/yggdrasil.mk
+	INCLUDES += $(LOCAL_PATH)/lk/common_mtk
+	INCLUDES += $(LOCAL_PATH)/lk/yggdrasil
+else ifeq ($(PLATFORM),yggdrasilx)
+    include $(LOCAL_PATH)/lk/common/common.mk
+	include $(LOCAL_PATH)/lk/common_mtk/common_mtk.mk
+	include $(LOCAL_PATH)/lk/yggdrasilx/yggdrasilx.mk
+	INCLUDES += $(LOCAL_PATH)/lk/common_mtk
+	INCLUDES += $(LOCAL_PATH)/lk/yggdrasilx
+else ifeq ($(PLATFORM),cedric)
+    include $(LOCAL_PATH)/lk/common/common.mk
+	include $(LOCAL_PATH)/lk/common_mtk/common_mtk.mk
+	include $(LOCAL_PATH)/lk/yggdrasil/yggdrasil.mk
+	INCLUDES += $(LOCAL_PATH)/lk/common_qcom
+	INCLUDES += $(LOCAL_PATH)/lk/cedric
+else
+    $(error Your target is either not set or is not supported)
+endif
