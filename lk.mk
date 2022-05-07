cae0201fc371fe7d736b460c51aad6f0d1ea7917
nift4 <nift4@protonmail.com>
2022-05-07 15:57:37 +0200

    kill makefile

--- lk.mk
+++ lk.mk
@@ -1,1 +1,14 @@
-include $(LOCAL_PATH)/lk/$(PLATFORM)/product.mk
+LOCAL_PATH := $(shell pwd)
+PLATFORM := $(strip $(PLATFORM))
+
+LK_DEVICES := cedric yggdrasil yggdrasilx
+
+ifeq (,$(PLATFORM))
+    $(error Please define the target device)
+endif
+
+ifneq (,$(filter $(LK_DEVICES), $(PLATFORM)))
+    include $(LOCAL_PATH)/lk/$(PLATFORM)/product.mk
+else
+    $(error Unsupported target device)
+endif
