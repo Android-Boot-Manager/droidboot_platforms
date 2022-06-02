fcc3deb02201d179432b262deaf3208bd54f90c3
Dom Rodriguez <shymega@shymega.org.uk>
2022-06-02 20:36:24 +0100

    Move includes to corresponding header file
    Felt more logical than the previous approach.
    
    Submitting as PR for further discussion on TG/GH.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/droidboot_platforms_common.c
+++ common/droidboot_platforms_common.c
@@ -1,18 +1,1 @@
-#if defined(PLATFORM_YGGDRASIL)
-#include <lk_mtk_common.h>
-#include <yggdrasil.h>
-#elif defined(PLATFORM_YGGDRASILX)
-#include <lk_mtk_common.h>
-#include <yggdrasilx.h>
-#elif defined(PLATFORM_CEDRIC)
-#include <lk_qcom_common.h>
-#include <yggdrasil.h>
-#elif defined(PLATFORM_PRO1)
-#include <droidboot_uefi_common.h>
-#include <common_abl.h>
-#include <pro1.h>
-#elif defined(PLATFORM_PRO1X)
-#include <droidboot_uefi_common.h>
-#include <common_abl.h>
-#include <pro1x.h>
-#endif
+#include <droidboot_platforms_common.h>
