5113f000ca2a83ca1a9b3e56ea6a0c7c341c1bcc
Dom Rodriguez <shymega@shymega.org.uk>
2022-08-15 22:44:01 +0100

    Remove unnecessary whitespace
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- common/droidboot_platform_common.h
+++ common/droidboot_platform_common.h
@@ -33,7 +33,7 @@ bool droidboot_key_read(lv_indev_drv_t * drv, lv_indev_data_t*data);
 
 int droidboot_platform_settings_dev_open(struct ext4_blockdev *bdev);
 int droidboot_platform_settings_dev_close(struct ext4_blockdev *bdev);
-			  
+
 droidboot_error droidboot_platform_init();
 ssize_t dridboot_sd_read_block(void *buf, uint32_t block, uint count);
 ssize_t dridboot_sd_write_block(const void *buf, uint32_t block, uint count);
@@ -47,4 +47,5 @@ void droidboot_lvgl_threads_init();
 
 void droidboot_platform_on_screen_log(const char *buf);
 void droidboot_platform_system_log(const char *buf);
+
 #endif
