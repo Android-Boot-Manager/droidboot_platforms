4e9ff3dc22a7a6b8c8b5c2d0b8a3b5ee8b3a99df
luka177 <lukapanio@gmail.com>
2022-09-25 10:05:44 +0300

    Refactor cedric to msm8953-lk2nd

--- lk/msm8953-lk2nd/product.mk
+++ lk/msm8953-lk2nd/product.mk
@@ -1,2 +1,2 @@
 include $(DROIDBOOT_PLATFORM_LOCAL_PATH)/lk/common_qcom/common.mk
-INCLUDES += $(DROIDBOOT_PLATFORM_LOCAL_PATH)/lk/cedric/src
+INCLUDES += $(DROIDBOOT_PLATFORM_LOCAL_PATH)/lk/msm8953-lk2nd/src
