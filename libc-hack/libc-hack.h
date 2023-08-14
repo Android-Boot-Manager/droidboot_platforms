098be943443e585d7d4ceb4dfea5b46d1bdd17c9
luka177 <lukapanio@gmail.com>
2023-08-14 21:15:02 +0200

    libc-hack: Make sure we have INT32_MAX

--- libc-hack/libc-hack.h
+++ libc-hack/libc-hack.h
@@ -1,3 +1,5 @@
+#ifndef LIBC_HACK_H
+#define  LIBC_HACK_H
 #if defined (PLATFORM_UBOOT)
 #include <linux/types.h>
 #include <stdlib.h>
@@ -6,3 +8,9 @@
 typedef int status_t;
 #elif defined (PLATFORM_UEFI)
 #endif
+
+// Common defenition (always safeguard with ifndef)
+#ifndef INT32_MAX
+#define INT32_MAX   0x7fffffff
+#endif
+#endif //  LIBC_HACK_H
